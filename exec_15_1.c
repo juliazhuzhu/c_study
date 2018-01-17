38578b9d839061824bed349b4bd51fb73987abeb
xiaoye <3144605@qq.com>
2018-01-17 15:22:40 +0800

    programming excercises chapter 15 .1

---
+++ exec_15_1.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h> 
+#include <limits.h> 
+#include <math.h>
+#include <string.h>
+
+//Write a function that converts a binary string to a numeric value.
+
+int calculate(int times){
+    int result =1;
+
+    result <<= times;
+    /*for (int i = 0; i < times; i++){
+        
+        result = result* 2;
+    }*/
+
+    return result;
+}
+
+int covnert_bin(char* input){
+
+    int len = strlen(input);
+    int i = 0;
+    int result = 0 ;
+    int tmp = 0;
+    int bit_val = 1;
+    for (i = len-1; i >=0; i--){
+       /* if (input[i] != '0' && input[i] != '1')
+        {
+            perror("input err \n");
+            return -1;
+        }*/
+        if (input[i] == '1')
+        {
+            //bit_val << 
+            result+=calculate(len-i-1);
+            printf("in process i %d result %d \n",i, result);
+        }
+    }
+
+    return result;
+
+
+
+}
+
+int main(){
+
+    char * pbin = "01001000";
+    int result = covnert_bin(pbin);
+
+    printf("result %d \n", result);
+
+    return 0;
+}
